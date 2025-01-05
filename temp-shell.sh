973c0afa6ed38aa0a192b903d164f7342645c62a
Martin <martinjonsson01@gmail.com>
2025-01-05 15:57:27 +0000

    Auto-setup temp-shell for psql backup fiddling

--- temp-shell.sh
+++ temp-shell.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-kubectl run temp-shell --rm -i --tty --image ubuntu --privileged -- bash
+kubectl run temp-shell --rm -i --tty --image ubuntu --privileged -- bash -c "apt-get update; DEBIAN_FRONTEND=noninteractive apt-get install -y postgresql-client nfs-common; mkdir /mnt/backups;mount -t nfs 192.168.0.162:/volume1/backups /mnt/backups; cd /mnt/backups;$SHELL"
