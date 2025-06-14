2a5a3f36c0791e3358d4b0a347adeaeaa67f107d
Martin <martinjonsson01@gmail.com>
2025-06-14 11:50:35 +0200

    Update to nixpkgs 25.05

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     talhelper.url = "github:budimanjojo/talhelper?ref=v3.0.19";
   };
 
