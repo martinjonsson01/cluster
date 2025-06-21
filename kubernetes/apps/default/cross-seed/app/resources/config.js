42bce3d217a4d7b42b65125dc1ee7af982589797
Martin <martinjonsson01@gmail.com>
2025-06-21 17:58:52 +0200

    Add tracker

--- kubernetes/apps/default/cross-seed/app/resources/config.js
+++ kubernetes/apps/default/cross-seed/app/resources/config.js
@@ -25,6 +25,7 @@ module.exports = {
     19, // FearNoPeer
     20, // YUSCENE
     23, // AnimeLovers
+    25, // PrivateHD
   ].map(i => `http://prowlarr.default.svc.cluster.local/${i}/api?apikey=${process.env.PROWLARR_API_KEY}`),
   /**
   * URL(s) to your Sonarr instance(s), included in the same way as torznab
