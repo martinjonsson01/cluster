2523b5c2044b8f3f3073c10ebb4d44dc0cddc1c3
Martin <martinjonsson01@gmail.com>
2025-11-01 15:17:48 +0100

    Make nsenter script more ergonomic

--- nsenter-talos.sh
+++ nsenter-talos.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
-set -x
 
 node=${1}
 #nodeName=$(kubectl get node ${node} -o template --template='{{index .metadata.labels "kubernetes.io/hostname"}}')
 nodeSelector='"nodeSelector": { "kubernetes.io/hostname": "'${node:?}'" },'
 podName=${USER}-nsenter-${node}
-kubectl -n kube-system run ${podName:?} --restart=Never -it --rm --image overriden --overrides '
+kubectl -n kube-system run "${podName:?}" --restart=Never -it --privileged --rm --image overriden --overrides '
 {
   "spec": {
     "hostPID": true,
@@ -17,8 +16,8 @@ kubectl -n kube-system run ${podName:?} --restart=Never -it --rm --image overrid
     "containers": [
       {
         "name": "nsenter",
-        "image": "mirror.gcr.io/library/busybox:musl",
-        "command": ["sh", "-c", "mkdir -p /host/var/lib/busybox; cp -r /bin/busybox /host/var/lib/busybox/; export PATH=\"$PATH:/var/lib/busybox\"; /host/var/lib/busybox/busybox --install /host/var/lib/busybox; nsenter -t1 -m -u -i -n /var/lib/busybox/busybox sh"],
+        "image": "debian:bookworm",
+        "command": ["bash", "-c", "apt update; DEBIAN_FRONTEND=noninteractive apt install -y usbutils nut nut-client nut-server; bash"],
         "stdin": true,
         "tty": true,
         "securityContext": {
